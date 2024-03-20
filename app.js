9fb0ab30b108e04fd1cdcab76eed5c28da987f3f
seon-b <86736916+seon-b@users.noreply.github.com>
2024-03-20 19:08:26 -0400

    Created app.js and style.css

---
+++ app.js
@@ -0,0 +1,7 @@
+let addItemButton = document.querySelector(".addItemButton");
+let searchButton = document.querySelector(".searchButton");
+let deleteAllButton = document.querySelector(".clearItems");
+
+let itemInputComponent = document.querySelector("#addItem");
+let searchItemInputComponent = document.querySelector("#filterItems");
+let itemsList = document.querySelector(".itemsList");
