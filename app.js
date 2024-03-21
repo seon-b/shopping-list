eac2d7c3ad9c33d4cd4dd0940374ce1cd15858af
seon-b <86736916+seon-b@users.noreply.github.com>
2024-03-21 11:42:08 -0400

    Added icon to remove list item

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ let itemInputComponent = document.querySelector("#addItem");
 let searchItemInputComponent = document.querySelector("#filterItems");
 let itemsList = document.querySelector(".itemsList");
 
+let removeItemIconClasses = "material-symbols-outlined removeItemIconStyle";
+
+const createItemIcon = (iconClasses) => {
+  let newItemIcon = document.createElement("span");
+  let newItemIconName = document.createTextNode("close");
+
+  newItemIcon.className = iconClasses;
+  newItemIcon.appendChild(newItemIconName);
+
+  return newItemIcon;
+};
+
 const addItemToList = () => {
   if (itemInputComponent.value.length === 0) {
     alert("Error, Please enter an item");
@@ -13,12 +25,18 @@ const addItemToList = () => {
   }
 
   let newItem = document.createElement("li");
-  let textData = document.createTextNode(itemInputComponent.value);
+  let newItemName = document.createTextNode(itemInputComponent.value);
 
-  newItem.appendChild(textData);
+  newItem.appendChild(newItemName);
+  newItem.appendChild(createItemIcon(removeItemIconClasses));
   newItem.classList.add("shoppingItem");
   itemsList.appendChild(newItem);
   itemInputComponent.value = "";
 };
 
+const removeItemFromList = (e) => {
+  e.target.remove();
+};
+
 addItemButton.addEventListener("click", addItemToList);
+itemsList.addEventListener("click", removeItemFromList);
