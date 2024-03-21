f085663f98e8683e5dd155b2b7b2390c47921098
seon-b <86736916+seon-b@users.noreply.github.com>
2024-03-20 21:31:26 -0400

    Removed form component

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ let deleteAllButton = document.querySelector(".clearItems");
 let itemInputComponent = document.querySelector("#addItem");
 let searchItemInputComponent = document.querySelector("#filterItems");
 let itemsList = document.querySelector(".itemsList");
+
+const addItemToList = () => {
+  if (itemInputComponent.value.length === 0) {
+    alert("Error, Please enter an item");
+    return;
+  }
+
+  let newItem = document.createElement("li");
+  let textData = document.createTextNode(itemInputComponent.value);
+
+  newItem.appendChild(textData);
+  newItem.classList.add("shoppingItem");
+  itemsList.appendChild(newItem);
+  itemInputComponent.value = "";
+};
+
+addItemButton.addEventListener("click", addItemToList);
